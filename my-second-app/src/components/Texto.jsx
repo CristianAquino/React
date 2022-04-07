602234e32a032ea3668224c3944dc8b7fafaabbb
cristianaquino <ycristian10@gmail.com>
2022-04-07 18:19:57 -0500

    txt

---
+++ my-second-app/src/components/Texto.jsx
@@ -0,0 +1,17 @@
+export function Texto() {
+  return (
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, culpa.
+      Facilis earum dolores harum ipsam ipsum error totam delectus deleniti
+      necessitatibus similique sit illo sed tempora possimus pariatur provident
+      minus quisquam, praesentium vero deserunt maiores tempore? Incidunt
+      quaerat illum dolores facere adipisci sint, quis inventore atque ipsum
+      aspernatur, harum mollitia totam dolor dignissimos consequuntur ex impedit
+      sunt eaque eos dolorem? Nam nisi labore eum praesentium vitae quasi ipsum
+      accusantium, doloribus, magnam cum ullam optio voluptas laboriosam culpa?
+      Nam ducimus animi cum. Aut ea molestiae alias mollitia. Commodi adipisci
+      laboriosam fugit quae neque quia. Aspernatur similique molestias dicta,
+      recusandae dolorem repellendus!
+    </p>
+  );
+}
