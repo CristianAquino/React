4b528fcc58e8b04117a23902609b9e2cc5f24a4d
cristianaquino <ycristian10@gmail.com>
2022-04-11 15:33:52 -0500

    app

---
+++ my-second-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Home } from "./components/Home";
+import { Class4useState } from "./components/Class4useState";
+import { Class5useEffect } from "./components/Class5useEffect";
+import { Class6useRef } from "./components/Class6useRef";
+import { Class7memoMemoCallback } from "./components/Class7memoMemoCallback";
+import { Class9Modal } from "./components/Class9Modal";
+import Class10ContextAPI from "./components/Class10ContextAPI";
+import { Class11useReducer } from "./components/Class11useReducer";
+import { Class12stateGlobal } from "./components/Class12stateGlobal";
+
+export function App() {
+  return (
+    <Fragment>
+      <Home />
+      {/* <Class4useState /> */}
+      {/* <Class5useEffect /> */}
+      {/* <Class6useRef /> */}
+      {/* <Class7memoMemoCallback /> */}
+      {/* <Class9Modal /> */}
+      {/* <Class10ContextAPI /> */}
+      {/* <Class11useReducer /> */}
+      {/* <Class12stateGlobal /> */}
+    </Fragment>
+  );
+}
