60b85798338a27bbd383f5abde9ff0590bd7b6a2
cristianaquino <ycristian10@gmail.com>
2022-04-11 15:33:28 -0500

    home

--- my-second-app/src/components/Imagen.jsx
+++ my-second-app/src/components/Imagen.jsx
@@ -1,8 +1,9 @@
 import logo from "../logo.svg";
+import style from "./Information.module.css";
 
 export function Imagen() {
   return (
-    <div className="imgBx">
+    <div className={style.imgBx}>
       <img src={logo} alt="" />
     </div>
   );
