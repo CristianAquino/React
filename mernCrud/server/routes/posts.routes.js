f4204e0638405ea7eed49cd08e8b9a322f991d60
cristianaquino <ycristian10@gmail.com>
2022-04-17 00:43:02 -0500

    server-routes

---
+++ mernCrud/server/routes/posts.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  createPosts,
+  deletePosts,
+  getPost,
+  getPosts,
+  updatePosts,
+} from "../controllers/posts.controllers.js";
+const router = Router();
+
+router.get("/posts", getPosts);
+router.post("/posts", createPosts);
+router.put("/posts/:id", updatePosts);
+router.delete("/posts/:id", deletePosts);
+router.get("/posts/:id", getPost);
+
+export default router;
