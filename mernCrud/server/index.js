86c053bc77a5989f3cf7d2ec89a1a64de372870d
cristianaquino <ycristian10@gmail.com>
2022-04-17 00:42:16 -0500

    server-index

---
+++ mernCrud/server/index.js
@@ -0,0 +1,11 @@
+// import cors from "cors";
+import { connectDB } from "./db.js";
+import { PORT } from "./config.js";
+import app from "./app.js";
+
+connectDB();
+
+// app.use(cors());
+
+app.listen(PORT);
+console.log("Server in running port 3000");
